f8eb68139919365fe20e00090c6c6c72fd29736f
vandeurenglenn <vandeurenglenn@gmail.com>
2022-07-03 17:36:45 +0200

    Update test contracts to solidity 0.8.15

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.15;
 import './../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract TestContract is ERC20 {
