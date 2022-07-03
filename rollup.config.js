4e337adb3b47310dd99b6651c1a88a6a58d0da5c
vandeurenglenn <vandeurenglenn@gmail.com>
2022-07-03 17:17:49 +0200

    Clean dist before build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,11 @@
 import json from  '@rollup/plugin-json'
 import { execSync } from 'child_process'
 
+try {
+	execSync('rm -rf ./dist/*')
+} catch (e) {
+
+}
 
 export default [{
 		input: ['./src/project-deploy.js', './src/tasks/tasks.js', './src/utils.js'],
